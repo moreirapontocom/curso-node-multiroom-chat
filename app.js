5323a517743c25262adae1b810cb395f2390f142
Lucas Moreira <moreirapontocom@gmail.com>
2019-09-05 22:08:35 -0300

    Installed socker.io and put it to listen at same port as node

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var app = require('./config/server');
 
-app.listen(4200, function() {
+var server = app.listen(4200, function() {
     console.log('Server is online');
 });
+
+require('socket.io').listen(server);
